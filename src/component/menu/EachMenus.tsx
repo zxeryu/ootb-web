79053d8d87929119a1377f89c536fd92bed8edaa
zx <zxpstudy@163.com>
2020-06-23 22:24:04 +0800

    fix: add antd menus

--- src/component/menu/EachMenus.tsx
+++ src/component/menu/EachMenus.tsx
@@ -3,25 +3,21 @@ import { IRouteMeta } from "../../core/route";
 import { Menu } from "antd";
 import { size, map } from "lodash";
 
-export const EachMenu = ({ route }: { route: IRouteMeta }) => {
-  // if (route.children && size(route.children) > 0) {
-  //   return (
-  //     <Menu.SubMenu title={route.title}>
-  //       {map(route.children, (sub) => (
-  //         <EachMenu route={sub} />
-  //       ))}
-  //     </Menu.SubMenu>
-  //   );
-  // }
-  return <Menu.Item key={route.path}>{route.title}</Menu.Item>;
-};
-
 export const EachMenus = ({ route }: { route: IRouteMeta }) => {
   return (
     <Menu mode={"inline"}>
-      {map(route.children, (sub) => (
-        <Menu.Item key={sub.path}>{sub.title}</Menu.Item>
-      ))}
+      {map(route.children, (sub) => {
+        if (size(sub.children) > 0) {
+          return (
+            <Menu.SubMenu key={sub.path} title={sub.title}>
+              {map(sub.children, (ss) => {
+                return <Menu.Item key={ss.path}>{ss.title}</Menu.Item>;
+              })}
+            </Menu.SubMenu>
+          );
+        }
+        return <Menu.Item key={sub.path}>{sub.title}</Menu.Item>;
+      })}
     </Menu>
   );
 };
